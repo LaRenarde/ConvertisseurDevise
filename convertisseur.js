0db233dc97802adf0b2e2daaf93be5c718f144db
Votre vrai nom ou pseudoSandra <sandra.organista.delonglee@gmail.com>
2020-08-25 11:49:38 +0200

    test

--- convertisseur.js
+++ convertisseur.js
@@ -5,28 +5,40 @@ document.addEventListener('DOMContentLoaded', function() {
     let euro = 10;
 
     let dollarUS = document.getElementById('dollarUS');
-        dollarUS = 1.18;
-
 
     let livreUK = document.getElementById('livreUK');
-        livreUK = 0.9;
-
 
     let dollarAUD = document.getElementById('dollarAUD');
-        dollarAUD = 1.64;
-
 
     let dollarCAD = document.getElementById('dollarCAD');
-        dollarCAD = 1.56;
-
 
     let japaneseYen = document.getElementById('japaneseYen');
-        japaneseYen = 125.23;
 
-    let convertMonney = document.getElementById('monney-choice').addEventListener('change', function(e) {
-        console.log(e);
+
+    for(nom in data) {
+        let monneyChoice = document.getElementById('monney-choice');
+        let option = document.createElement("option");
+            monneyChoice.appendChild(option)
+            option.innerHTML = pays.value
+    };
+
+
+        
+
        
+
+
     });
+
+
+    
+    
+    
+    /*.addEventListener('change', function(e) {
+        if (f) {
+            dollarUS.value 
+        }
+    });*/
 
 
 
